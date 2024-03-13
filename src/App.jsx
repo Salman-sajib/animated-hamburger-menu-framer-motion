a8d7f992a9e8b32e5fdfbb64136d050a44dede12
Salman-sajib <sfsajib@gmail.com>
2024-03-13 15:16:05 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import AnimatedHamburger from './components/AnimatedHamburger';
+
+function App() {
+  return (
+    <div className='grid min-h-dvh place-content-center bg-gradient-to-br from-violet-500 to-indigo-500'>
+      <AnimatedHamburger />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AnimatedHamburger.jsx
@@ -0,0 +1,7 @@
+import { motion } from 'framer-motion';
+
+function AnimatedHamburger() {
+  return <div>AnimatedHamburger</div>;
+}
+
+export default AnimatedHamburger;
